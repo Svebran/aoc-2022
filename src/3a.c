aace56d8d7e3b23e5644e7497b69fbf59dceab9c
Svebran <36282193+Svebran@users.noreply.github.com>
2022-12-03 10:21:18 +0100

    Day 3

--- src/3a.c
+++ src/3a.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <inttypes.h>
 
 int main()
 {
-    printf("This solution is yet to be made.\n");
+    uint64_t sum = 0, bitmapL, bitmapR;
+    char n;
+    char buffer[1024];
+    while(fgets(buffer, 1024, stdin) != NULL)
+    {
+        bitmapL = 0; bitmapR = 0; n = 0;
+        while(buffer[n] != '\n') n++;
+        for(int i = 0; i < n/2; i++)
+            bitmapL |= (uint64_t)1 << (buffer[i] <= 'Z' ? 26+buffer[i]-'A' : buffer[i]-'a');
+        for(int i = n/2; i < n; i++)
+            bitmapR |= (uint64_t)1 << (buffer[i] <= 'Z' ? 26+buffer[i]-'A' : buffer[i]-'a');
+        sum += __builtin_ffsll(bitmapL & bitmapR);
+    }
+    printf("%"PRIu64"\n", sum);
     return 0;
 }
--- src/3b.c
+++ src/3b.c
@@ -1,7 +1,37 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <inttypes.h>
 
 int main()
 {
-    printf("This solution is yet to be made.\n");
+    uint64_t sum = 0, bitmap1, bitmap2, bitmap3;
+    char n;
+    char buffer[1024];
+    while(fgets(buffer, 1024, stdin) != NULL)
+    {
+        bitmap1 = 0; bitmap2 = 0; bitmap3 = 0;
+        n = 0;
+        while(buffer[n] != '\n')
+        {
+            bitmap1 |= (uint64_t)1 << (buffer[n] <= 'Z' ? 26+buffer[n]-'A' : buffer[n]-'a');
+            n++;
+        }
+        fgets(buffer, 1024, stdin);
+        n = 0;
+        while(buffer[n] != '\n')
+        {
+            bitmap2 |= (uint64_t)1 << (buffer[n] <= 'Z' ? 26+buffer[n]-'A' : buffer[n]-'a');
+            n++;
+        }
+        fgets(buffer, 1024, stdin);
+        n = 0;
+        while(buffer[n] != '\n')
+        {
+            bitmap3 |= (uint64_t)1 << (buffer[n] <= 'Z' ? 26+buffer[n]-'A' : buffer[n]-'a');
+            n++;
+        }
+        sum += __builtin_ffsll(bitmap1 & bitmap2 & bitmap3);
+    }
+    printf("%"PRIu64"\n", sum);
     return 0;
 }
