c5836c3f77a2e5029bb944831f24613ece2f9610
Svebran <36282193+Svebran@users.noreply.github.com>
2022-12-04 10:31:33 +0100

    Day 4

--- src/4b.c
+++ src/4b.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <inttypes.h>
 
 int main()
 {
-    printf("This solution is yet to be made.\n");
+    uint64_t count = 0, r1B, r1E, r2B, r2E;
+    char buffer[1024];
+    while(fgets(buffer, 1024, stdin) != NULL)
+    {
+        sscanf(buffer, "%"SCNu64"-%"SCNu64",%"SCNu64"-%"SCNu64, &r1B, &r1E, &r2B, &r2E);
+        if(r1B <= r2E && r1E >= r2B)
+            count++;
+        else if(r2B <= r1E && r2E >= r1B)
+            count++;
+    }
+    printf("%"PRIu64"\n", count);
     return 0;
 }
