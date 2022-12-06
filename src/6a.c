cd341d17af566528ae90ea78a7aee93f4ae9075a
Svebran <36282193+Svebran@users.noreply.github.com>
2022-12-06 23:37:46 +0100

    Day 6

--- src/6a.c
+++ src/6a.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <stdlib.h>
+
+uint64_t counts[256];
 
 int main()
 {
-    printf("This solution is yet to be made.\n");
+    uint64_t duplicate_count = 0, buffer_count = 0;
+    char prebuffer[1025+4];
+    char* buffer = prebuffer+4;
+    int64_t i = 0;
+    fgets(buffer, 1025, stdin);
+    while(i < 4)
+        duplicate_count+=++counts[buffer[i++]]==2;
+    do
+    {
+        while(i < 1024)
+            duplicate_count != 0
+                ? duplicate_count+=(++counts[buffer[i]]==2)-(--counts[buffer[i++-4]]==1)
+                : (printf("%"PRId64"\n",i+buffer_count*1024),exit(0));
+        i = 0, buffer_count++;
+        prebuffer[0] = buffer[1020], prebuffer[1] = buffer[1021], prebuffer[2] = buffer[1022], prebuffer[3] = buffer[1023];
+    }
+    while(fgets(buffer, 1025, stdin) != NULL);
     return 0;
 }
--- src/6b.c
+++ src/6b.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <stdlib.h>
+
+uint64_t counts[256];
 
 int main()
 {
-    printf("This solution is yet to be made.\n");
+    uint64_t duplicate_count = 0, buffer_count = 0;
+    char prebuffer[1025+14];
+    char* buffer = prebuffer+14;
+    int64_t i = 0;
+    fgets(buffer, 1025, stdin);
+    while(i < 14)
+        duplicate_count+=++counts[buffer[i++]]==2;
+    do
+    {
+        while(i < 1024)
+            duplicate_count != 0
+                ? duplicate_count+=(++counts[buffer[i]]==2)-(--counts[buffer[i++-14]]==1)
+                : (printf("%"PRId64"\n",i+buffer_count*1024),exit(0));
+        i = 0, buffer_count++;
+        for(uint64_t j = 0; j < 14; j++)
+            prebuffer[j] = buffer[1024-14+j];
+    }
+    while(fgets(buffer, 1025, stdin) != NULL);
     return 0;
 }
